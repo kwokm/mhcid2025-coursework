a17e4efd50cfedc480be6704d0ed3453f8bc1a22
Michael Kwok <michaelgkwok@gmail.com>
2025-03-21 11:19:29 -0700

    CP6 checkpoint applied to CP6 folder and better organization and sublinking overall

--- q2winter/hcid521-prototyping/cp6-mvp/raspberry-pi-code/scripts/first_time_setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# WORK IN PROGRESS
-
-
-# Keep the script running (optional - remove if not needed)
-# This prevents the terminal from closing immediately after execution
-# read -p "Press Enter to exit..."
--- q2winter/hcid521-prototyping/cp7-finaldemo/raspberry-pi-code/ARCHIVE/jscode/justListen.js
+++
@@ -1,8 +0,0 @@
-import { wordsToAudio } from '../../gemini-prompts/wordsToAudioGeminiPrompt';
-import { startListening, updateAudioFiles, initializeAudioFiles } from './buttonactionKeyboard';
-async function main() {
-  initializeAudioFiles();
-  startListening();
-}
-
-main();
